f59b07997ac38412dce0961f46355fa0af33118b
kkksskkss <75146563+kkksskkss@users.noreply.github.com>
2024-03-25 18:47:23 -0300

    Add files via upload

---
+++ 111/move_file.py
@@ -0,0 +1,13 @@
+
+
+
+from_dir(downloads)
+to_dir(Arquivos_Documentos)
+
+list_of_files = os.listdir(fromdir)
+print(list_of_files) 
+
+if list('')
+
+if.path.exists(path2):
+    print("caminho de origem" + from_dir "/" + file_name.)
